328a7fbbe29109d94dbb721b2888380699d6c6ef
SamuelAA21 <e.samuel.aa05@gmail.com>
2025-02-01 23:02:57 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regalo Sorpresa</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="gift" id="gift">
+            <img src="gift-closed.png" alt="Regalo Cerrado" id="gift-image">
+        </div>
+        <div class="dancing-doll" id="dancing-doll">
+            <img src="doll.png" alt="Muñequito Bailando">
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
